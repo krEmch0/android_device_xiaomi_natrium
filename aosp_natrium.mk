222eeeab67d5937c7f2502a7ff754a7ddc355e2d
krEmch0 <emchovasilev127@outlook.com>
2025-03-20 13:31:34 +0200

    Update and rename lineage_natrium.mk to aosp_natrium.mk

--- aosp_natrium.mk
+++ aosp_natrium.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/natrium/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_natrium
